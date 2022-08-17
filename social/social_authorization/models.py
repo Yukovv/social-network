a55b5186e858b6456121f98247c945eed400db2e
user <JIK0009@yandex.ru>
2022-08-17 15:54:42 +0700

    add UserProfile model

--- social/social_authorization/models.py
+++ social/social_authorization/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+from django.dispatch import receiver
+
+UserModel: User = get_user_model()
+
+
+class UserProfile(models.Model):
+    user: UserModel = models.OneToOneField(
+        UserModel,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    birthday = models.DateField()
+    city = models.CharField(max_length=30, blank=True)
+    gender = models.CharField(max_length=10, null=True)
+    bio = models.TextField(max_length=300, blank=True)
+    occupation = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} profile"
+
+
+@receiver(post_save, sender=UserModel)
+def user_saved_handler(instance: UserModel, created: bool, **kwargs):
+    if not created:
+        return
+
+    UserProfile.objects.create(user=instance)
