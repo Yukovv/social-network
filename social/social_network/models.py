5a64b56c6972f59bfca8e21d84ed6a8b9c3f3330
user <JIK0009@yandex.ru>
2022-08-18 15:38:47 +0700

    add post model

--- social/social_network/models.py
+++ social/social_network/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from social_authorization.models import UserModel
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(blank=True, max_length=2000)
+    img = models.ImageField(null=True, upload_to='images/posts')
+    likes = models.ManyToManyField(UserModel)
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, null=True, related_name="posts")
+
+    def __str__(self):
+        return self.title
