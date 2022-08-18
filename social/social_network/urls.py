3271c6c5a1202fb048c7a279fb2770ff0c15d438
user <JIK0009@yandex.ru>
2022-08-18 15:39:33 +0700

    add profile view

--- social/social_network/urls.py
+++ social/social_network/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 
+from .views import (
+    UserProfileView,
+)
+
 app_name = "social_network"
+
+urlpatterns = [
+    path('', UserProfileView.as_view(), name="profile")
+]
--- social/social_network/views.py
+++ social/social_network/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import UserModel, Post
+from social_authorization.models import UserProfile
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    queryset = UserModel.objects.select_related('userprofile_set')
+    template_name = "social_network/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_posts"] = Post.objects.filter(user=self.object).prefetch_related('likes')
+        return context
+
